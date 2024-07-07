1fbee214cf44bb18fdecfa2f5c6c1193f5521c10
mtalalned <muhammadtalalned@gmail.com>
2024-07-07 00:27:15 -0700

    15 questions completed

---
+++ app.js
@@ -0,0 +1,160 @@
+// IF ELSE ELSE IF STATEMENT
+
+// Question No. 1
+// var inputNumber = +prompt ('Enter a number');
+// if (inputNumber%3 == 0){
+//     console.log ('number is divisible by 3')
+// }
+// else {
+//     console.log ('number is not divisible by 3')
+// }
+
+// // Question No. 2
+// var inputNumber = Number(prompt('Enter a number'));
+// if (inputNumber%2 == 0) {
+//     console.log ('the number is even')
+// }
+// else {
+//     console.log ('number is odd')
+// }
+
+// // Question No. 3
+// var variableAge = +prompt ('Enter your age');
+// if (variableAge > 18){
+//     console.log ('Old enough')
+// }
+// else {
+//     console.log ('Too Young')
+// }
+
+// Question No. 4
+// var yourName = prompt('Enter your name in small letters');
+// var myName = 'talal';
+
+// if (yourName === myName){
+//     console.log ('Welcome to my website')
+// }
+// else {
+//     console.log (" ");
+// }
+
+// Question No. 5
+// var inputNumber = +prompt ('Enter a number');
+// switch (inputNumber%3){
+//     case 0:
+//         console.log ('number is divisible by 3')
+//     break;
+//     default:
+//         console.log ('number is not divisible by 3')
+//     }
+
+// Quesiton No. 6
+// var input = prompt('Enter')
+
+// Question No. 7
+// var num1 = +prompt ('Enter first number');
+// var num2 = +prompt ('Enter second number');
+// var operator = prompt ('Enter operation\n"+" for addition\n"-" for subtraction\n"/" for division\n"*" for multiplication');
+
+// if (operator === '+'){
+//     console.log (num1+num2)
+// }else if (operator === '-'){
+//     console.log (num1-num2)
+// }else if(operator === '/'){
+//     console.log (num1/num2)
+// }else if(operator === '*'){
+//     console.log (num1*num2)
+// } else {
+//     console.log ('Enter correct operator')
+// }
+
+// Question No. 8
+// var inputTime = +prompt ('Enter time in 24 hours format\n For example: 1900 = 7pm');
+// if (inputTime>= 0 && inputTime<1200){
+//     console.log ('Good Morning')
+// } else if (inputTime>= 1200 && inputTime<1700){
+//     console.log ('Good Afternoon')
+// }else if (inputTime>= 1700 && inputTime<2100){
+//     console.log ('Good Evening')
+// }else if (inputTime>= 2100 && inputTime<=2359){
+//     console.log ('Good night')
+// }else {
+//     console.log ('Enter time value as instructed')
+// }
+
+// Question No. 9
+// var year = +prompt ('Enter year in YYYY format');
+// if (year%4 === 0) {
+//     console.log ('It is a leap year')
+// }else {
+//     console.log ('Its not a leap year')
+// }
+
+// Question No. 10
+// var correctPasssword = 'password123';
+// var inputPassword = prompt('Enter password');
+// if (inputPassword == 0) {
+//     alert ('Please enter password')
+// }else if (inputPassword===correctPasssword){
+//     alert ('Entered password is correct')
+// }else {
+//     alert ('You have entered an incorrect password')
+// }
+
+// Question No. 11
+// var firstName = "Ali";
+// if (firstName === "Fahad") {
+// document.write("Hello Fahad!");
+// }else {
+//     document.write ('You are not Fahad')
+// }
+
+// Quesiton No. 12
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";}
+// else{
+// greeting = "Good evening";
+// }
+
+// Quesiton No. 13
+// var num1 = +prompt ('Enter first number');
+// var num2 = +prompt ('Enter second number');
+// if (num1 > num2 ){
+//     console.log (num1)
+// }else if (num1 === num2){
+//     console.log ('Numbers are equal')
+// }else (
+//     console.log (num2)
+// )
+
+// Question No. 14
+// var inputNumber = +prompt ('Enter a number');
+// if (inputNumber>0){
+//     console.log ('Positive')
+// }else if (inputNumber < 0){
+//     console.log ('Negative')
+// }else {
+//     console.log (inputNumber)
+// }
+
+// Question No. 15
+
+
+
+// Question No. 16
+// var value = 15;
+
+
+
+// Question No. 17
+// var inputCharacter = prompt('Enter a single alphabet')
+// console.log (inputCharacter ==='a' || inputCharacter ==='e' || inputCharacter ==='i' || inputCharacter ==='o' || inputCharacter ==='u' || inputCharacter ==='A' || inputCharacter ==='E' || inputCharacter ==='I' || inputCharacter ==='O' || inputCharacter ==='U' )
+
+// Question No. 18
+// console.log (10 !== 8)
+
+// Question No. 19
+
+// Question No. 20
